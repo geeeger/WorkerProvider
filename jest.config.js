dc2b4c5fd837353cdbef91e8376304fec93e06e2
geeeger <lph1988520@sina.com>
2019-01-19 16:21:43 +0800

    test: fix tsconfig

--- jest.config.js
+++ jest.config.js
@@ -28,7 +28,8 @@ module.exports = {
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
-    "/node_modules/"
+    "/node_modules/",
+    "/lib/"
   ],
 
   // A list of reporter names that Jest uses when writing coverage reports
@@ -64,7 +65,7 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     "ts-jest": {
-      "tsConfigFile": "tsconfig.json"
+      "tsConfig": "tsconfig.json"
     }
   },
 
